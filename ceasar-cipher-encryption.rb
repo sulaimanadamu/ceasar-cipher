4000e30b1f3e22945da63141f143faa427c577ba
sulaiman adamu <adamusulaiman2021@gmail.com>
2025-08-15 00:38:30 +0100

    this doesn't follow case encryption

--- ceasar-cipher-encryption.rb
+++ ceasar-cipher-encryption.rb
@@ -1,32 +1,35 @@
 
+
 def caesar_cipher(string, shift)
-   
-    upper_case_ascii_range = ['A'..'Z'].map(&:ord)
-    lower_case_ascii_range = ['a'..'z'].map(&:ord)
+    $result = ""
 
-    result = ""
-    
+    upper_case_ascii_range = ('A'..'Z').map(&:ord)
+    lower_case_ascii_range = ('a'..'z').map(&:ord)
+    ascii_characters = [ upper_case_ascii_range, lower_case_ascii_range].flatten()
+
+    for character in string.chars
+        index = ascii_characters.index(character.ord)
         
-
-    for character in string
-        character_ascii = character.ord
-        if upper_case_ascii_range.include? (character_ascii + shift)
-            result += character_ascii.ord
-        elsif lower_case_ascii_range.include? (character_ascii + shift)
-            result += character_ascii.ord
+        if index.nil?
+             $result << character
+        else
+            code_index = index + shift
+            coded_character = ascii_characters[code_index]
+            $result << coded_character.ord
         end
-    end
+    end 
+    return $result
 end
 
-print "Enter string to encode: "
-print " "
-string = gets.chomp
+# print "Enter string to encode: "
+# print " "
+# string = gets.chomp
 
-print "Shift by: "
-print " "
-number = gets.chomp
+# print "Shift by: "
+# print " "
+# shift = gets.chomp
 
-
+p caesar_cipher("What a string!", 5)
 
 
 
