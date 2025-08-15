1cf2091e2e03fca329f4a8ef7829a74062e730fd
sulaiman adamu <adamusulaiman2021@gmail.com>
2025-08-15 01:17:56 +0100

    Follows the rules, of saving upper case

--- ceasar-cipher-encryption.rb
+++ ceasar-cipher-encryption.rb
@@ -5,17 +5,38 @@ def caesar_cipher(string, shift)
 
     upper_case_ascii_range = ('A'..'Z').map(&:ord)
     lower_case_ascii_range = ('a'..'z').map(&:ord)
-    ascii_characters = [ upper_case_ascii_range, lower_case_ascii_range].flatten()
+    
 
     for character in string.chars
-        index = ascii_characters.index(character.ord)
+        char_ascii = character.ord
+        extended_ascii = char_ascii + shift
         
-        if index.nil?
-             $result << character
+        if upper_case_ascii_range.include?(char_ascii)
+            if upper_case_ascii_range.include?(extended_ascii)
+                $result << extended_ascii.ord
+            else
+                # done to get the number of characters we have to count to get our encoded character
+                reminder = extended_ascii - upper_case_ascii_range[-1]
+
+                # find the index of the character, we have to remember about zero index here.
+                reminder_index = reminder - 1
+
+                $result << upper_case_ascii_range[reminder_index].ord
+            end
+            
+        elsif lower_case_ascii_range.include?(char_ascii)
+            if lower_case_ascii_range.include?(extended_ascii)
+                $result << extended_ascii.ord
+            else
+                # done to get the number of characters we have to count to get our encoded character
+                reminder = extended_ascii - lower_case_ascii_range[-1]
+
+                # find the index of the character, we have to remember about zero index here.
+                reminder_index = reminder - 1
+                $result << lower_case_ascii_range[reminder_index].ord
+            end
         else
-            code_index = index + shift
-            coded_character = ascii_characters[code_index]
-            $result << coded_character.ord
+             $result << character
         end
     end 
     return $result
